61754c818b384f88d25a83ab1faf3e36238c923b
Frederick Kim <freddy.kim8@gmail.com>
2021-03-15 04:22:01 -0700

    export all frames in current page

--- src/code.ts
+++ src/code.ts
@@ -1,20 +1,72 @@
-figma.showUI(__html__)
+// figma.showUI(__html__)
 
-figma.ui.onmessage = msg => {
-  if (msg.type === 'create-rectangles') {
-    const nodes = []
+// figma.ui.onmessage = msg => {
+//   if (msg.type === 'create-rectangles') {
+//     const nodes = []
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle()
-      rect.x = i * 150
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
-      figma.currentPage.appendChild(rect)
-      nodes.push(rect)
+//     for (let i = 0; i < msg.count; i++) {
+//       const rect = figma.createRectangle()
+//       rect.x = i * 150
+//       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
+//       figma.currentPage.appendChild(rect)
+//       nodes.push(rect)
+//     }
+
+//     figma.currentPage.selection = nodes
+//     figma.viewport.scrollAndZoomIntoView(nodes)
+//   }
+
+//   figma.closePlugin()
+// }
+
+const { children } = figma.currentPage;
+
+function hasValidSelection(nodes) {
+  return !(!nodes || nodes.length === 0);
+}
+
+export interface ExportableBytes {
+  name: string;
+  setting: ExportSettingsImage | ExportSettingsPDF | ExportSettingsSVG;
+  bytes: Uint8Array;
+}
+
+async function main(nodes: readonly SceneNode[]): Promise<string> {
+  if (!hasValidSelection(children))
+    return Promise.resolve("Nothing prototyped for export");
+
+  let exportableBytes: ExportableBytes[] = [];
+  for (let node of nodes) {
+    let { name, exportSettings } = node;
+    // Should we force to use PNG or use the export setting?
+    if (exportSettings.length === 0) {
+      exportSettings = [
+        {
+          format: "PNG",
+          suffix: "",
+          constraint: { type: "SCALE", value: 1 },
+          contentsOnly: true,
+        },
+      ];
     }
 
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
+    for (let setting of exportSettings) {
+      let defaultSetting = setting;
+      const bytes = await node.exportAsync(defaultSetting);
+      exportableBytes.push({
+        name,
+        setting,
+        bytes,
+      });
+    }
   }
 
-  figma.closePlugin()
+  figma.showUI(__html__, { visible: false });
+  figma.ui.postMessage({ exportableBytes });
+
+  return new Promise((resolve) => {
+    figma.ui.onmessage = () => resolve("Complete");
+  });
 }
+
+main(children).then((res) => figma.closePlugin(res));
